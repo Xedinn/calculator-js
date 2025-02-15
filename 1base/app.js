e1f032be798290b7f263c415e352d6aabf4798af
Xedinn <Xedinn90@mail.ru>
2025-02-15 20:09:52 +0300

    добавляет обработчик кнопок + и -

--- 1base/app.js
+++ 1base/app.js
@@ -26,7 +26,11 @@ alert (firstName)
 const resultElement = document.getElementById('result')
 const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
-const submitBtn = document.getElementById('submit')   
+const submitBtn = document.getElementById('submit') 
+const plusBtn = document.getElementById('plus') 
+const minusBtn = document.getElementById('minus') 
+
+let action = '+'
 
 // console.log(resultElement.textContent)
 // resultElement.textContent = 42
@@ -34,11 +38,25 @@ const submitBtn = document.getElementById('submit')
 
 // console.log(typeof sum);
 
-submitBtn.onclick = function () {
-    const sum  = Number(input1.value) + Number(input2.value)
-resultElement.textContent = sum
-    }
 
+plusBtn.onclick = function () {
+action = '+'
+}
+
+minusBtn.onclick = function () {
+    action = '-'
+}
+
+submitBtn.onclick = function () {
+
+    if (action == '+') {
+        const sum  = Number(input1.value) + Number(input2.value)
+        resultElement.textContent = sum 
+    } else {
+        const sum  = Number(input1.value) - Number(input2.value)
+        resultElement.textContent = sum 
+    }
+}
 
 
     
