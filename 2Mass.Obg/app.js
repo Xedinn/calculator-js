c168e94d02a94f087b5c8e39b465430cec06e3ff
Xedinn <Xedinn90@mail.ru>
2025-03-12 23:41:28 +0300

    теория массивов 1

---
+++ 2Mass.Obg/app.js
@@ -0,0 +1,18 @@
+// Theory
+
+// const array = [1, 2, 3, 5, 20, 42, 111]
+// // const arrayString = ['a', 'b', 'c', null, 12]
+
+// // const array = new Array(1, 2, 3, 5, 20, 42)
+
+// // console.log(array.length);
+// // console.log(array[1]);
+// // console.log(array[5]);
+// // console.log(array[10]);
+// // console.log(array[array.length - 1]); //array [6]
+
+// array[0] = "Privet"
+// console.log(array);
+// array[array.length] = 'becon'
+
+
