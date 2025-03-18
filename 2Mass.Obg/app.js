0e2ee3aad0a1c1d8ce3a55197187c109cf18a08b
Xedinn <Xedinn90@mail.ru>
2025-03-18 23:57:36 +0300

    реализованы добавления и удаления с изменением анимации кнопки

--- 2Mass.Obg/app.js
+++ 2Mass.Obg/app.js
@@ -94,6 +94,9 @@ const notes = [
 
 function render() {
   listElement.innerHTML=""
+  if (notes.length === 0) {
+    listElement.innerHTML = "<p> Нет элементов</p>"
+  }
   for (let i = 0;i < notes.length; i++) {
       listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i], i))
 
@@ -114,13 +117,26 @@ render()
 inputElement.value = ""
 };
 
+listElement.onclick = function (event) {
+if (event.target.dataset.index) {
+const index = parseInt(event.target.dataset.index)
+const type = event.target.dataset.type
+
+if (type==="toggle"){
+notes[index].completed = !notes[index].completed
+} else if (type=== "remove")
+notes.splice(index, 1)
+}
+render()
+}
+
 function getNoteTemplate(note, index) {
   return `
     <li class="list-group-item d-flex justify-content-between align-items-center">
         <span class = "${note.completed ? "text-decoration-line-through" : ""}">${note.title}</span> <!-- Текст заметки -->
         <span>
-            <span class="btn btn-small btn-${note.completed ? "warning" : "success"}" data-index="${index}">&check;</span> <!-- Кнопка "Готово" -->
-            <span class="btn btn-small btn-danger">&times;</span> <!-- Кнопка "Удалить" -->
+            <span class="btn btn-small btn-${note.completed ? "warning" : "success"}" data-index="${index}" data-type="toggle">&check;</span> <!-- Кнопка "Готово" -->
+            <span class="btn btn-small btn-danger" data-type="remove" data-index=${index}>&times;</span> <!-- Кнопка "Удалить" -->
         </span>
     </li>
   `;
