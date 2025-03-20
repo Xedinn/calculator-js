2482c7c0a85340b10ae2d28422e52d7b02d381f9
Xedinn <Xedinn90@mail.ru>
2025-03-20 23:51:08 +0300

    практика методов массивов

---
+++ 2Mass.Obg/array-theory.js
@@ -0,0 +1,17 @@
+const names = ["Влад", "Елена","Игорь","Ксения"]
+
+// names.push("Алена")
+// names.unshift("Алена")
+
+// const firstName = names.shift()
+// const name = names.pop()
+// console.log("Names:", names, name);
+
+// console.log(names.reverse());
+
+const reversed= names.toReversed()
+console.log(reversed);
+console.log(names)
+
+
+
